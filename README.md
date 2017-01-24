683c56eba8a9566b3007c1c058014313a6a7674f
Jo Liss <joliss42@gmail.com>
2017-01-24 15:34:32 +0000

    Benchmark rendering as well

--- README.md
+++ README.md
@@ -12,7 +12,14 @@ node index.js
 With Node v6.9.0 on Linux:
 
 ```
-lex: 1803.342ms
-parse: 291.767ms
-codeGen: 2462.688ms
+lex: 1753.248ms
+parse: 251.063ms
+codeGen: 2304.035ms
+runtimeWrap: 6.797ms
+render: 6.182ms
 ```
+
+## Note
+
+Don't use this code as an example of how to use the Pug API. We're misusing it
+and skipping steps to drill down into the timings for each component.
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let lex = require('pug-lexer')
 let parse = require('pug-parser')
 let link = require('pug-linker')
 let codeGen = require('pug-code-gen')
+var runtimeWrap = require('pug-runtime/wrap')
 var util = require('util')
 var fs = require('fs')
 
@@ -12,7 +13,7 @@ var contents = fs.readFileSync(fullPath, 'utf8')
 
 var n = 200;
 
-var tokens, ast, linkedAst, code;
+var tokens, ast, linkedAst, code, fn, html;
 
 console.time('lex')
 for (var i = 0; i < n; i++) {
@@ -24,6 +25,7 @@ console.timeEnd('lex')
 
 console.time('parse')
 for (var i = 0; i < n; i++) {
+  // parse mutates the tokens, so we clone with .slice.
   ast = parse(tokens.slice(0), {
     filename: fullPath,
     src: contents
@@ -38,3 +40,18 @@ for (var i = 0; i < n; i++) {
   code = codeGen(linkedAst, { compileDebug: false });
 }
 console.timeEnd('codeGen')
+
+console.time('runtimeWrap')
+for (var i = 0; i < n; i++) {
+  fn = runtimeWrap(code)
+}
+console.timeEnd('runtimeWrap')
+
+console.time('render')
+for (var i = 0; i < n; i++) {
+  html = fn();
+}
+console.timeEnd('render')
+
+// Check that we actually rendered the whole thing:
+//console.log(html)
